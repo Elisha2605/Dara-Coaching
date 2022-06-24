f1f925c3febe90e3b70d995f89d534405e83ca3c
Elisha2605 <elisha_ngoma@yahoo.fr>
2022-06-24 23:09:43 +0200

    trying to fix eventlistener on available times

--- api/GET/fetch_booking_data_GET.py
+++ api/GET/fetch_booking_data_GET.py
@@ -31,14 +31,6 @@ def _():
         cursor.execute(sql_fetchAll_booking_options)
         booking_options = cursor.fetchall()
 
-        # sql_fetchAll_booking_date_time =    """ 
-        #                                         SELECT bkg_date_time_id, booking_dates.available_dates, available_times
-        #                                         FROM booking_date_times
-        #                                         JOIN booking_dates ON booking_date_times.fk_bkg_date_id = booking_dates.bkg_date_id 
-        #                                     """
-        # cursor.execute(sql_fetchAll_booking_date_time)
-        # booking_date_times = cursor.fetchall()
-
         sql_fetchAll_booking_date_times =    """ 
                                                 SELECT * FROM booking_date_times 
                                              """
@@ -62,4 +54,40 @@ def _():
             ), default=str)
     except Exception as ex:
         print(ex)
+       
+
+
+
+#### test1
+@get('/test/<date>')
+def _(date):
+
+    try:
+
+        ################  CONNECT TO DATABASE  ###################
+        db_connection = mysql.connector.connect(**DATABASE_CONFIG)
+        cursor = db_connection.cursor(dictionary=True)
+        ##########################################################
+
+     
+        sql_fetchAll_booking_dates_with_times =    f""" 
+                                                        SELECT DISTINCT bkg_date_time_id, booking_dates.available_dates, available_times 
+                                                        FROM booking_date_times
+                                                        INNER JOIN booking_dates ON booking_date_times.fk_bkg_date_id = booking_dates.bkg_date_id
+                                                        WHERE booking_dates.available_dates = {date} 
+                                                   """
+        cursor.execute(sql_fetchAll_booking_dates_with_times)
+        booking_dates_times = cursor.fetchall()
         
+        print('#'*100)
+        print(booking_dates_times)
+
+
+        response.content_type = 'application/json; charset=UTF-8'
+        return json.dumps(dict(
+            booking_dates_and_times=booking_dates_times
+            ), default=str)
+
+    except Exception as ex:
+        print(ex)
+       
--- static/js/booking.js
+++ static/js/booking.js
@@ -4,8 +4,28 @@ const participantsSelector = document.querySelector("#selector-participants")
 const optionsSelector = document.querySelector("#selector-options")
 const timesSelector = document.querySelector("#selector-times")
 const datesSelector = document.querySelector("#selector-dates")
+console.log(datesSelector);
 
 
+
+//test1
+datesSelector.addEventListener("change", (event) => {
+    let date = event.target.value
+    console.log(date);
+    fetch(`/test/"${date}"`)
+    .then(res => res.json())
+    .then(data => {
+       data.booking_dates_and_times.forEach(element => {
+        if (date === element.available_dates){
+            console.log(element.available_times);
+            const newOp = document.createElement("option");
+            newOp.append(element.available_times)
+            timesSelector.insertAdjacentElement("afterbegin", newOp)
+        }
+            
+       });
+    })         
+})
 
 // PARTICIPANTS
 //=======================================================================
@@ -13,29 +33,29 @@ fetch('/api-fetch-booking_data')
 .then(res => res.json())
 .then(data => {
     data.participants.forEach(element => {
-        console.log(element);
+        // console.log(element);
         const newOp = document.createElement("option");
         newOp.append(element.participants)
         participantsSelector.insertAdjacentElement("afterbegin", newOp)
     });
     data.booking_options.forEach(element => {
-        console.log(element);
+        // console.log(element);
         const newOp = document.createElement("option");
         newOp.append(element.options)
         optionsSelector.insertAdjacentElement("afterbegin", newOp)
     })
     data.booking_dates.forEach(element => {
-        console.log(element);
+        // console.log(element);
         const newOp = document.createElement("option");
         newOp.append(element.available_dates)
         datesSelector.insertAdjacentElement("afterbegin", newOp)
     })
-    data.booking_date_times.forEach(element => {
-        console.log(element);
-        const newOp = document.createElement("option");
-        newOp.append(element.available_times)
-        timesSelector.insertAdjacentElement("afterbegin", newOp)
-    })
+    // data.booking_date_times.forEach(element => {
+    //     // console.log(element);
+    //     const newOp = document.createElement("option");
+    //     newOp.append(element.available_times)
+    //     timesSelector.insertAdjacentElement("afterbegin", newOp)
+    // })
 })
 
 // CREATE CUSTOMER
@@ -50,7 +70,7 @@ async function create_customer() {
         alert("Could not connect")
         return
     }
-    console.log(form);
+    // console.log(form);
 }
 
 // UPDATE CUSTOMER
